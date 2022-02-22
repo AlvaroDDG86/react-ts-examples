2e7b8d88587031577e0d148a87730ca4f7e95447
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-02-22 17:08:33 +0100

    use first hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,40 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import UseStateComponent from "./components/UseStateComponent";
+import UseEffectComponent from "./components/UseEffectComponent";
+import UseContextComponent from "./components/UseContextComponent";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>React + typescript</h1>
+      <p>
+        <a
+          href="https://github.com/typescript-cheatsheets/react"
+          target="_blanck"
+        >
+          Cheatsheet
+        </a>
+      </p>
+      <div>
+        <h2>useState</h2>
+        <UseStateComponent />
+        <hr />
+      </div>
+      <div>
+        <h2>useState</h2>
+        <UseEffectComponent />
+        <hr />
+      </div>
+      <div>
+        <h2>useContext</h2>
+        <UseContextComponent />
+        <hr />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
